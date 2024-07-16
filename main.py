45e5a98a03264b51333b698ee0a0c581309bc5e9
TimaSv-Uk <sviridenkotimofei@gmail.com>
2024-07-16 11:38:37 +0300

    add look_and_say func

--- main.py
+++ main.py
@@ -2,7 +2,25 @@ def add(a: int, b: int):
     return a + b
 
 
+def look_and_say(data:str):
+    newString = ""
+    prev = ""
+    sameCharInRow = -1
+
+    for i in data:
+        if i == prev:
+            sameCharInRow += 1
+        else:
+            if sameCharInRow > 0:
+                newString += str(sameCharInRow) + prev
+            prev = i
+            sameCharInRow = 1
+    newString +=  str(sameCharInRow) + prev
+    return newString
+
 def main():
+    data = "1113222113"
+    print(look_and_say(data))
     add(1, 2)
 
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 import unittest
-from main import add
+from main import add,look_and_say
 
 
 class TestMain(unittest.TestCase):
@@ -7,6 +7,17 @@ class TestMain(unittest.TestCase):
         self.assertEqual(add(1, 2), 3)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
+
+    def test_look_and_say(self):
+        # 1 becomes 11 (1 copy of digit 1).
+        # 11 becomes 21 (2 copies of digit 1).
+        # 21 becomes 1211 (one 2 followed by one 1).
+        # 1211 becomes 111221 (one 1, one 2, and two 1s).
+        # 111221 becomes 312211 (three 1s, two 2s, and one 1).
+        self.assertEqual(look_and_say("1"), "11")
+        self.assertEqual(look_and_say("11"), "21")
+        self.assertEqual(look_and_say("21"), "1211")
+        self.assertEqual(look_and_say("111221"), "312211")
 
 
 if __name__ == "__main__":
